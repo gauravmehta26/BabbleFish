a4b5eba5c9fcb76a4c6b17414032b6337b3aac21
gauravmehta <gaurav.mehta@nl.abnamro.com>
2019-10-11 15:09:34 +0200

    Initial commit

---
+++ JS/voice-translator-config.js
@@ -0,0 +1,3 @@
+var bucketName = 'babelfish-stack-voicetranslatorbucket-1p6tzqsgjtypl';
+var IdentityPoolId = 'eu-west-1:8598d185-b4b6-4b58-a60c-e81454dc4d9f';
+var lambdaFunction = 'babelfish-stack-VoiceTranslatorLambda-I9C9LFVQFCQG';
