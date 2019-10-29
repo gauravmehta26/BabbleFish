933186396a941d85f99b99d78816dffef4c334b8
gauravmehta <gaurav.mehta@nl.abnamro.com>
2019-10-29 13:50:55 +0100

    optimised to use better permission

--- voice-translator-config.js
+++ voice-translator-config.js
@@ -1,4 +1,5 @@
-var bucketName = 'babbelfish-app-bucket1';
-var IdentityPoolId = 'eu-west-1:90a1d897-d046-447a-87f6-8ec14924a6b0';
-var lambdaFunction = 'babelfishstack-VoiceTranslatorLambda-CDOG76I7H6IU';
-var awsRegion = 'eu-west-1';
+//Get values for all these fields from the output of cloud formation stack
+var bucketName = <bucket created by cloud formation>;
+var IdentityPoolId = <cognito pool id created by cloud formation>;
+var lambdaFunction = <lambda function name created by cloud formation>;
+var awsRegion = <region where your stack is created>;
